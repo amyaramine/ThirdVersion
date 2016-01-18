b2aa10d3f858b9c038cdfe78321c710d3bcd0ad4
amyaramine <amyaramine@gmail.com>
2016-01-18 09:56:33 +0100

    Initialisation

---
+++ UniversityThirdVersion/src/main/java/com/example/UniversityThirdVersionApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityThirdVersionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversityThirdVersionApplication.class, args);
+	}
+}
